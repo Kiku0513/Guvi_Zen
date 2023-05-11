ccc0093053d3a47b229acc3c7b0b3bbc8dc39f12
Komal <komaldhumale9333@gmail.com>
2023-05-11 11:29:32 +0530

    Added comments

--- src/github_session_/Guvi_Zen_Learning.java
+++ src/github_session_/Guvi_Zen_Learning.java
@@ -1,7 +1,8 @@
 package github_session_;
-
+//Learning git n github
 public class Guvi_Zen_Learning
-{public static void main(String[] args)
+{
+	public static void main(String[] args)
 {
 	System.out.println("I am Komal Nikame");
 	System.out.println("good morning");
--- src/github_session_/Hello_Java.java
+++ src/github_session_/Hello_Java.java
@@ -2,6 +2,7 @@ package github_session_;
 //Demo Program
 public class Hello_Java
 {
+	
 	public static void main(String[] args)
 	{
 		System.out.println(" i m komal");
